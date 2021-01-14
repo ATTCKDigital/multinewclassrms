10ae863f027d308c6df93a2dda0b556300139c53
Ergin Deniz Kosecioglu <deniz@crafton.pl>
2021-01-14 16:09:59 +0100

    general styling and compoenent added

--- wp-content/themes/newclassrooms/js/components.js
+++ wp-content/themes/newclassrooms/js/components.js
@@ -1,6 +1,8 @@
 import FLEX from 'FLEX/js/clientNamespace';
 // import your child components
+import Nav from '../components/component_nav/nav';
 
 // then add them to this object
 FLEX.ChildComponents = {
+  'Nav': Nav,
 };
