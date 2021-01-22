7a3723cf83baa76bbf228f8ba6302ee5b598bf54
Dennis Plucinik <dennis.plucinik@gmail.com>
2021-01-21 23:55:50 -0500

    adding WP Engine environment variables

--- wp-content/themes/newclassrooms/functions.php
+++ wp-content/themes/newclassrooms/functions.php
@@ -1,14 +1,15 @@
 <?php
 // 'prod', 'stage', etc. are the names of  WPEngine environments
 // 'WPE_PROD' & 'WPE_STAGE' are key names of the WPENGINE_ACCOUNT' server variable
-define('WPE_PROD', 'prod');
-define('WPE_STAGE', 'stage');
+define('WPE_PROD', 'ncttomultisite');
+define('WPE_STAGE', 'ncttomultistg');
 
-define('CHILD_THEME_DIR', get_stylesheet_directory()); // use when there are files that should ONLY be from the child theme.
+// Use when there are files that should ONLY be from the child theme.
+define('CHILD_THEME_DIR', get_stylesheet_directory());
 // Use locate_template() to include files.  This function first checks the child theme for the file and if there is none, uses the parent theme.
 // Allows us to override main functions in the child theme without changing the parent.
 
-/*** Global Variables ***/
+// Global Variables
 // These define globally available variables, and must be included first
 include_once(locate_template('config/global-variables/blocks.php'));
 include_once(locate_template('config/global-variables/colors.php'));
@@ -17,28 +18,29 @@ include_once(locate_template('config/global-variables/blocks.php'));
 include_once(locate_template('config/global-variables/wysiwyg-formats.php'));
 
 
-// WP functions are split out into individual files for clarity. Disable/Enable files by commenting out here. 
+// WP functions are split out into individual files for clarity. 
+// Disable/Enable files by commenting out here. 
 // See README.md in parent theme for details on each config file
 
-/*** ACF Configs ***/
+// ACF
 // include_once(locate_template('config/acf-configs/acf-field-values.php'));
 // include_once(locate_template('config/acf-configs/acf-wpml-options.php'));
 include_once(locate_template('config/acf-configs/acf-options-page.php')); //REQUIRED
 include_once(locate_template('config/acf-configs/acf-search.php')); //RECOMMENDED
 
-/*** WP-Admin Configs ***/
+// WP-Admin
 // include_once(locate_template('config/admin-configs/change-post-labels.php'));
 // include_once(locate_template('config/admin-configs/image-crops.php')); //RECOMMENDED
 include_once(locate_template('config/admin-configs/remove-comments-column.php')); //RECOMMENDED
 // include_once(locate_template('config/admin-configs/sidebars.php'));
 
-/*** Theme Configs ***/
+// Theme 
 // include_once(locate_template('config/theme-configs/custom-post-types.php'));
 // include_once(locate_template('config/theme-configs/customizer-colors.php')); //REQUIRED
 // include_once(locate_template('config/theme-configs/disable-tax-archive.php'));
 // include_once(locate_template('config/theme-configs/geotarget.php'));
+// include_once(locate_template('config/theme-configs/password-protection.php'));
 include_once(locate_template('config/theme-configs/load-more.php')); //RECOMMENDED
 include_once(locate_template('config/theme-configs/author-slug.php'));
-// include_once(locate_template('config/theme-configs/password-protection.php'));
 include_once(locate_template('config/theme-configs/wpml-language-switcher.php'));
 
