7ab7f39af42fe340bcac4076ba8ff4ed98804ecd
Dennis Plucinik <dennis.plucinik@gmail.com>
2021-01-21 23:56:53 -0500

    adding ColorTile component

--- wp-content/themes/newclassrooms/js/components.js
+++ wp-content/themes/newclassrooms/js/components.js
@@ -1,18 +1,21 @@
 import FLEX from 'FLEX/js/clientNamespace';
-// import your child components
+
+// FLEX components
 import Nav from '../components/component_nav/nav';
 import Carousel from '../components/component_carousel/carousel';
+import ColorTile from '../components/component_colortile/colortile';
 import VideoPopup from '../components/component_video-popup/video-popup';
 
-// Blocks
-import TestimonialCarousel from '../gutenberg/blocks/block_testimonialcarousel/testimonialcarousel';
+// Gutenberg blocks
 import HeroSlider from '../gutenberg/blocks/block_hero-slider/hero-slider';
+import TestimonialCarousel from '../gutenberg/blocks/block_testimonialcarousel/testimonialcarousel';
 
 // then add them to this object
 FLEX.ChildComponents = {
   'Carousel': Carousel,
+  'ColorTile': ColorTile,
+  'HeroSlider': HeroSlider,
   'Nav': Nav,
-  'VideoPopup': VideoPopup,
   'TestimonialCarousel': TestimonialCarousel,
-  'HeroSlider': HeroSlider
+  'VideoPopup': VideoPopup
 };
