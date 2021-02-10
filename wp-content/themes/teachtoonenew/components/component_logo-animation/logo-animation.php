34e3e5941e4a5e5349cce58ff5a4b256b53d5b14
Ergin Deniz Kosecioglu <deniz@crafton.pl>
2021-02-10 17:55:14 +0100

    logo animation component fix

--- wp-content/themes/teachtoonenew/components/component_logo-animation/logo-animation.php
+++ wp-content/themes/teachtoonenew/components/component_logo-animation/logo-animation.php
@@ -1,4 +1,4 @@
-<div class="component-logo-animation" data-component-name="LogoAnimation">
+<div class="component component-logo-animation" data-component-name="LogoAnimation">
   <svg class="circle-svg animate" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
     <circle class="circle-stroke circle1-line" stroke="#FFD44A" fill-opacity="0" />
     <circle class="circle-stroke circle2-line" stroke="#0A99F7" fill-opacity="0" />
--- wp-content/themes/teachtoonenew/gutenberg/blocks/block_logo-animation/logo-animation.php
+++ wp-content/themes/teachtoonenew/gutenberg/blocks/block_logo-animation/logo-animation.php
@@ -13,6 +13,6 @@ $id = 'logo-animation-' . $block['id'];
 // Create align class ("alignwide") from block setting ("wide")
 $align_class = $block['align'] ? 'align' . $block['align'] : '';
 ?>
-<div class="component <?php echo $align_class; ?>" id="<?php echo $id; ?>" data-component-name="LogoAnimation">
+<div class="component <?php echo $align_class; ?>" id="<?php echo $id; ?>">
   <?php echo Utils::render_template('components/component_logo-animation/logo-animation.php'); ?>
 </div>
