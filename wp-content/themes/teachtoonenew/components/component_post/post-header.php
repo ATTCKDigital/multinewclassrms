36c945c8b79a8016568adfcbffca923fcd09955a
Ergin Deniz Kosecioglu <deniz@crafton.pl>
2021-02-10 08:14:16 +0100

    Styling fixes - Share Block Styling Added

--- wp-content/themes/teachtoonenew/components/component_post/post-header.php
+++ wp-content/themes/teachtoonenew/components/component_post/post-header.php
@@ -2,18 +2,16 @@
 	$category = get_the_category(); 
 	$category_slug = $category[0]->slug;
 ?>
-<section class="component-row flex-grid padding-tablet-landscape-bottom-8x padding-bottom-4x padding-tablet-landscape-top-4x margin-top-2x" data-logo-color="logo-color-dark">
-	<div class="component-row  component-background-center-center padding-tablet-landscape-top-8x  margin-top-8x  margin-tablet-landscape-top-2x " data-section-id="937">
+<section class="component-post-header component-row flex-grid padding-tablet-landscape-bottom-8x padding-bottom-4x padding-tablet-landscape-top-4x margin-top-2x" data-logo-color="logo-color-dark">
+	<div class="component-row component-background-center-center padding-tablet-landscape-top-8x margin-top-8x margin-tablet-landscape-top-2x" data-section-id="937">
 		<div class="flex-grid component-row-wide component-alignment-top"></div>
 	</div>
 	<div class="column flex-12-12 text-align-center">
-		<div class="component-post-header">
-			<header class="single-header flex-tablet-landscape-8-12 flex-12-12">
-				<div class="color-text-primary display-block margin-bottom-2x margin-tablet-landscape-bottom-3x text-align-center category category-<?= $category_slug; ?>"></div>
-				<h1 class="headline2 title color-text-primary flex-tablet-landscape-8-12 flex-12-12 text-align-center margin-bottom-2x margin-tablet-landscape-bottom-3x"><?php the_title();?></h1>
-				<span class="eyebrow uppercase display-block text-align-center date"><?= get_the_time('F j, Y');?></span>
-			</header>
-		</div>
+		<header class="single-header flex-tablet-landscape-8-12 flex-12-12">
+			<div class="color-text-primary display-block margin-bottom-2x margin-tablet-landscape-bottom-3x text-align-center category category-<?= $category_slug; ?>"></div>
+			<h1 class="headline2 title color-text-primary flex-tablet-landscape-8-12 flex-12-12 text-align-center margin-bottom-2x margin-tablet-landscape-bottom-3x"><?php the_title();?></h1>
+			<span class="eyebrow uppercase display-block text-align-center date"><?= get_the_time('F j, Y');?></span>
+		</header>
 	</div>
 	<?php if(has_post_thumbnail()){ ?>
 		<div class="flex-grid component-row-full component-alignment-center padding-tablet-landscape-top-8x padding-top-4x">
