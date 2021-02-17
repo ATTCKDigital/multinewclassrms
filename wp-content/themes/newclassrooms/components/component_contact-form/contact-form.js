ed971674a5b962eaec6c8fe42b1d5f1c08323570
Dennis Plucinik <dennis.plucinik@gmail.com>
2021-02-17 03:27:32 -0500

    adjusting contact form from teach to one to new classrooms

--- wp-content/themes/newclassrooms/components/component_contact-form/contact-form.js
+++ wp-content/themes/newclassrooms/components/component_contact-form/contact-form.js
@@ -1,25 +1,36 @@
-import $ from 'jquery'
+import $ from 'jquery';
+import FLEX from '../../../FLEX/js/clientNamespace';
 
 class ContactForm {
     constructor($cf7) {
+    	console.log('/newclassrooms/\tcomponents/\tcomponent_contact_form/\tcontact-form.js', 'constructor()');
+
         this.$cf7 = $cf7
         this.$interestsCheckboxes = $cf7.find('[name="interested-in[]"]')
         this.$rolesDropdown = $cf7.find('[name="role"]')
     }
 
     init() {
+    	console.log('/newclassrooms/\tcomponents/\tcomponent_contact_form/\tcontact-form.js', 'init()');
+
         this.bindEvents()
-        this.prefillRoles()
+        // this.prefillRoles()
         this.prefillInterests()
     }
 
     bindEvents() {
+    	console.log('/newclassrooms/\tcomponents/\tcomponent_contact_form/\tcontact-form.js', 'bindEvents(), this.$interestsCheckboxes: this:', this.$interestsCheckboxes, this);
+
         this.$interestsCheckboxes.each((index, checkbox) => {
-            $(checkbox).change(this.syncHiddenInputs)
+        	console.log('/newclassrooms/\tcomponents/\tcomponent_contact_form/\tcontact-form.js › checkbox, index: checkbox: ', index, checkbox);
+
+        	$(checkbox).change(this.syncHiddenInputs)
         })
     }
 
     syncHiddenInputs(ev) {
+    	console.log('/newclassrooms/\tcomponents/\tcomponent_contact_form/\tcontact-form.js', 'syncHiddenInputs(ev:)', ev);
+
         const $checkbox = $(ev.currentTarget)
 
         // Slugify the selected value
@@ -39,8 +50,9 @@ class ContactForm {
     }
 
     prefillRoles() {
-        const urlParams = new URLSearchParams(location.search)
+    	console.log('/newclassrooms/\tcomponents/\tcomponent_contact_form/\tcontact-form.js', 'prefillRoles()');
 
+        const urlParams = new URLSearchParams(location.search)
         const role = urlParams.get('role')
         const hasRole = this.$rolesDropdown.find(`option:contains("${role}")`).length > 0
 
@@ -50,13 +62,13 @@ class ContactForm {
     }
 
     prefillInterests() {
-        const urlParams = new URLSearchParams(location.search)
+    	console.log('/newclassrooms/\tcomponents/\tcomponent_contact_form/\tcontact-form.js', 'prefillInterests()');
 
+        const urlParams = new URLSearchParams(location.search)
         const interest = urlParams.get('int')
 
         this.$interestsCheckboxes.each(function(index, checkbox) {
-
-            if(checkbox.value.toLowerCase() === interest.toLowerCase()) {
+            if (!FLEX.isUndefinedOrNull(interest) && interest.hasOwnProperty('toLowerCase') && checkbox.value.toLowerCase() === interest.toLowerCase()) {
                 checkbox.checked = true
                 $(checkbox).change()
             }
@@ -64,8 +76,8 @@ class ContactForm {
     }
 }
 
-
 const init = function() {
+	console.log('/newclassrooms/\tcomponents/\tcomponent_contact_form/\tcontact-form.js', 'init()');
 
     const $cf7 = $('.component-contact-form.component')
 
@@ -74,6 +86,5 @@ const init = function() {
         contactForm.init()
     }
 }
-
 
 export default init
