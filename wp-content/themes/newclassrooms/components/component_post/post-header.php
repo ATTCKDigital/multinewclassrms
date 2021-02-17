9af66435b2f3ced00edeb7fbe2877e4cc1444ee9
Ergin Deniz Kosecioglu <deniz@crafton.pl>
2021-02-17 11:49:41 +0100

    post header align center changed to text align center - circle centered

--- wp-content/themes/newclassrooms/components/component_post/post-header.php
+++ wp-content/themes/newclassrooms/components/component_post/post-header.php
@@ -9,15 +9,15 @@
 	<div class="column flex-tablet-landscape-10-12 flex-12-12">
 		<div class="component-post-header">
 			<header class="single-header">
-				<div class="color-text-primary display-block margin-bottom-2x margin-tablet-landscape-bottom-3x align-center category category-<?= $category_slug; ?>"></div>
-				<h1 class="headline2 color-text-primary align-center margin-bottom-2x margin-tablet-landscape-bottom-3x"><?php the_title();?></h1>
-				<span class="eyebrow uppercase display-block align-center date"><?= get_the_time('F j, Y');?></span>
+				<div class="color-text-primary display-block margin-bottom-2x margin-tablet-landscape-bottom-3x text-align-center category category-<?= $category_slug; ?>"></div>
+				<h1 class="headline2 color-text-primary text-align-center margin-bottom-2x margin-tablet-landscape-bottom-3x"><?php the_title();?></h1>
+				<span class="eyebrow uppercase display-block text-align-center date"><?= get_the_time('F j, Y');?></span>
 			</header>
 		</div>
 	</div>
 	<?php if(has_post_thumbnail()){ ?>
 		<div class="flex-grid component-row-full component-alignment-center padding-tablet-landscape-top-8x padding-top-4x">
-			<div class="column component-row-wide align-center">
+			<div class="column component-row-wide text-align-center">
 				<div class="component-post-header">
 					<div class="featured-image">
 						<?php the_post_thumbnail('full');?>
