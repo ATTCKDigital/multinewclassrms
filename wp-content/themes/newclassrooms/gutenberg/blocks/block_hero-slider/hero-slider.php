54b70b1683bc2381a68b73046245101099e28307
Dennis Plucinik <dennis.plucinik@gmail.com>
2021-02-23 01:45:40 -0500

    adding external link functinality to hero slider

--- wp-content/themes/newclassrooms/gutenberg/blocks/block_hero-slider/hero-slider.php
+++ wp-content/themes/newclassrooms/gutenberg/blocks/block_hero-slider/hero-slider.php
@@ -45,17 +45,34 @@
 				</div>
 				<div class="content padding-tablet-landscape-bottom-12x">
 					<div class="side-row" style="background-color: <?= $slide['accent_color'] ?>;"></div>
-					<h1 class="headline6 is-style-headline6 align-left"><?= $slide['subtitle']; ?></h1>
+					<h1 class="headline6 is-style-headline6 align-left flex-tablet-portrait-18-24">
+						<?= $slide['subtitle']; ?>
+					</h1>
 					<div class="headline-container">
-						<h2 class="headline1 is-style-headline1 align-left margin-top-1x flex-tablet-portrait-7-12">
+						<h2 class="headline1 is-style-headline1 align-left margin-top-1x flex-tablet-portrait-13-24">
 							<?= $slide['title'] ?>
 						</h2>
 					</div>
 					<?php if (!empty($slide['popup_name'])): ?>
 						<div class="component-button component align-left margin-top-4x margin-tablet-landscape-top-6x ">
-							<button class="cta video-button open-popup-button prepare-in-view" data-popup-content-id="<?= $slide['popup_name'] ?>">
-								<?php _e('Watch Video', '_flex') ?>
-							</a>
+							<?php 
+								// Initialize default vars
+								$slideText = __('Watch Video', '_flex');
+								$videoIconDisplayState = '';
+
+								// Override default CTA copy, if set by user.
+								if (!empty($slide['cta_text'])) {
+									$slideText = $slide['cta_text'];
+								}
+
+								// Only show video icon when CTA says "Watch Now".
+								if (strtolower($slideText) != 'watch now') {
+									$videoIconDisplayState = ' hideVideoIcon';
+								}
+							?>
+							<button class="cta video-button open-popup-button prepare-in-view<?= $videoIconDisplayState ?>" data-popup-content-id="<?= $slide['popup_name'] ?>">
+								<?= $slideText ?>
+							</button>
 						</div>
 					<?php endif; ?>
 					<nav class="slider-navigation margin-top-4x margin-tablet-landscape-top-8x">
