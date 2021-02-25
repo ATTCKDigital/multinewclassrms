4d5216c616392de4f68fc99341369d97eb1abe6c
Dennis Plucinik <dennis.plucinik@gmail.com>
2021-02-24 23:51:51 -0500

    adjusting post page styles

--- wp-content/themes/newclassrooms/components/component_post/post-header.php
+++ wp-content/themes/newclassrooms/components/component_post/post-header.php
@@ -2,11 +2,11 @@
 	$category = get_the_category(); 
 	$category_slug = $category[0]->slug;
 ?>
-<section class="component-row padding-tablet-landscape-bottom-8x padding-bottom-4x padding-tablet-landscape-top-4x margin-top-2x" data-logo-color="logo-color-dark">
+<section class="flex-grid component-row padding-tablet-landscape-bottom-8x padding-bottom-4x padding-tablet-landscape-top-4x margin-top-2x" data-logo-color="logo-color-dark">
 	<div class="component-row  component-background-center-center padding-tablet-landscape-top-8x  margin-top-8x  margin-tablet-landscape-top-2x " data-section-id="937">
 		<div class="flex-grid component-row-wide component-alignment-top"></div>
 	</div>
-	<div class="column flex-tablet-landscape-10-12 flex-12-12">
+	<div class="column flex-tablet-landscape-9-12 flex-12-12 block-align-center">
 		<div class="component-post-header">
 			<header class="single-header">
 				<div class="color-text-primary display-block margin-bottom-2x margin-tablet-landscape-bottom-3x text-align-center category category-<?= $category_slug; ?>"></div>
