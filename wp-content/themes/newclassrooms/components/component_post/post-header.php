eca51d18b3bd75ef9d4109415e7f9b46c6e5b01d
Dennis Plucinik <dennis.plucinik@gmail.com>
2021-03-24 09:32:35 -0400

    fixing link colors on mobile footer
    adjusting mobile font sizes on blog post page

--- wp-content/themes/newclassrooms/components/component_post/post-header.php
+++ wp-content/themes/newclassrooms/components/component_post/post-header.php
@@ -6,12 +6,12 @@
 	<div class="component-row  component-background-center-center padding-tablet-landscape-top-8x  margin-top-8x  margin-tablet-landscape-top-2x " data-section-id="937">
 		<div class="flex-grid component-row-wide component-alignment-top"></div>
 	</div>
-	<div class="column flex-tablet-landscape-9-12 flex-12-12 block-align-center">
+	<div class="column flex-11-12 flex-tablet-landscape-9-12 block-align-center">
 		<div class="component-post-header">
 			<header class="single-header">
 				<a href="/the-latest" class="back">&#x2190; Back</a>
 				<div class="color-text-primary display-block margin-bottom-2x margin-tablet-landscape-bottom-3x text-align-center category category-<?= $category_slug; ?>"></div>
-				<h1 class="headline2 color-text-primary text-align-center margin-bottom-2x margin-tablet-landscape-bottom-3x"><?php the_title();?></h1>
+				<h1 class="headline3 color-text-primary text-align-center margin-bottom-2x margin-tablet-landscape-bottom-3x"><?php the_title();?></h1>
 				<span class="eyebrow uppercase display-block text-align-center date"><?= get_the_time('F j, Y');?></span>
 				<span class="eyebrow uppercase display-block text-align-center author margin-top-2x"><?= get_field('author_name');?></span>
 			</header>
